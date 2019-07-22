b8ed7a7df97dcae4d4db2345e969dbf9317dd346
Helmut Kemper <helmut.kemper@gamil.com>
2019-07-21 21:19:35 -0300

    initial commit

--- server/new.server_test.go
+++ server/new.server_test.go
@@ -1,9 +1,13 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	iot_json "github.com/helmutkemper/iotmaker.server.json"
 	"github.com/xenolf/lego/log"
+	"io/ioutil"
 	"net/http"
+	"sync"
 )
 
 func test(w http.ResponseWriter, r *http.Request) {
@@ -34,9 +38,40 @@ func ExampleNewServer() {
 		},
 	}
 
-	log.Fatalf("server error: %v", NewServer(project))
+	var wg sync.WaitGroup
+	var toOut interface{}
+
+	wg.Add(1)
+	go func(project Project) {
+		wg.Done()
+		log.Fatalf("server error: %v", NewServer(project))
+	}(project)
+
+	wg.Wait()
+
+	r, err := http.Get("http://0.0.0.0:8080/test")
+	if err != nil {
+		log.Fatalf("test error: %v", err.Error())
+		return
+	}
+
+	fmt.Printf("status: %v\n", r.Status)
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatalf("test error: %v", err.Error())
+		return
+	}
+
+	err = json.Unmarshal(body, &toOut)
+	if err != nil {
+		log.Fatalf("test error: %v", err.Error())
+		return
+	}
+
+	fmt.Printf("%v", toOut.(map[string]interface{})["Objects"])
 
 	// Output:
-	//
+	// status: 200 OK
+	// [map[string:esta vivo]]
 
 }
